fc1824b482ac7924d0f421a91179d9c059840990
bstanger <beccastanger@gmail.com>
2018-01-15 12:14:17 -0800

    Pass stringifyJSON tests
    This work allows primitives as well
    as non-primitives to pass all tests
    to convert to a string.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (!obj) {
+    return 'null';
+  }
+  
+  if (Array.isArray(obj)) {
+    var outputArr = [];
+    
+    for( var i = 0; i < obj.length; i++){
+      outputArr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + outputArr + ']';
+    
+  } else if (typeof obj === 'object'){
+    var outputStr = '';    
+    for (var key in obj) {
+      console.log('key ' + key);
+      console.log('typeof key: ' + typeof(key));
+      if(typeof key === 'function' || typeof obj[key] === 'function' || key === undefined || obj[key] === undefined){
+        continue;
+      }
+      outputStr += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }    
+    return '{' + outputStr.slice(0, -1) + '}';
+  }
 };
